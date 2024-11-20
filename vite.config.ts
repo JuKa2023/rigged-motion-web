8c47bca4678036d5eadd256d93986fd2e6e62c84
Noah <thenoahleuenberger@gmail.com>
2024-11-20 13:39:47 +0100

    Added deploy script and config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/rigged-Motion-Studios_Diggezz2/",
   resolve: {
     alias:{
       "@": path.resolve(__dirname, "./src"),
